4173c94b153fc350a16c9b6b3fe859498361bf2e
ahmadreza2343 <argolmakany@gmail.com>
2024-05-13 00:46:37 +0430

    rtl and adduser modal

--- assest/Js/usermanager.js
+++ assest/Js/usermanager.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
+
+
+    const adduserbtn = document.getElementById("add_user_btn");
+    adduserbtn.addEventListener(
+      "click" , ()=>{
+         document.querySelector(".add_user_modal").classList.add("show");
+         document.querySelector(".add_user_modal_back").classList.remove("dis-none");
+        
+      })
+        document.querySelector(".add_user_modal_back").addEventListener("click" , ()=>{
+          document.querySelector(".add_user_modal").classList.remove("show");
+          document.querySelector(".add_user_modal_back").classList.add("dis-none");
+        });
   });
+
+
