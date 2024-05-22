22cd10f62ab6c738d9e7fef3a101ebfd1a858d3e
ahmadreza2343 <argolmakany@gmail.com>
2024-05-22 19:40:15 +0430

    pagination

--- assest/Js/alpinegsController/userscontroller.js
+++ assest/Js/alpinegsController/userscontroller.js
@@ -1,15 +1,59 @@
 document.addEventListener("alpine:init", () => {
-    Alpine.data("usersData", function () {
-        return{
-            users:[],
-            getUsers(){
-                axios.get('https://jsonplaceholder.typicode.com/users').then((res)=>{
-                    this.users=res.data;
-                    console.log(res);
-                })
-            }
+    Alpine.data("usersData", () => ({
+
+        users: [],
+        pageUsers: [],
+        isloading: false,
+        showModal: false,
+        pageCount: 1,
+        itemsCount: 4,
+        currentPage: 1,
+        getUsers() {
+            this.isloading = true;
+            axios.get('https://jsonplaceholder.typicode.com/users').then((res) => {
+                this.users = res.data;
+                this.pagination();
+            }).catch(error => {
+                // console.log(error.message);
+                alert(error.message);
+            }).finally(() => {
+                this.isloading = false;
+
+            })
+        },
+        pagination() {
+
+            this.pageCount = Math.ceil(this.users.length / this.itemsCount);
+            let start = (this.currentPage * this.itemsCount) - this.itemsCount;
+            let end = this.currentPage * this.itemsCount
+            this.pageUsers = this.users.slice(start, end)
+            console.log(this.pageUsers);
+
+        },
+        nextPage() {
+            this.currentPage++;
+            if (this.currentPage > this.pageCount) this.currentPage = this.pageCount;
+
+            this.pagination();
+
+        },
+        prevPage(){
+            this.currentPage--;
+            if (this.currentPage < 1) this.currentPage = 1;
+
+            this.pagination();
+
+        },
+        handleChangeItemsCount(value){
+            this.currentPage = 1;
+            if (value < 1) this.itemsCount = 1;
+            
+            if(value > this.users.length) this.itemsCount = this.users.length;
+            // this.pagination();
 
         }
-
     })
+
+
+    )
 })
--- assest/Js/usermanager.js
+++ assest/Js/usermanager.js
@@ -3,50 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
   var instances = M.Collapsible.init(elems, {});
 
 
-  const adduserbtn = document.getElementById("add_user_btn");
-  adduserbtn.addEventListener(
-    "click", () => {
-      document.querySelector(".add_user_modal").classList.add("show");
-      document.querySelector(".add_user_modal_back").classList.remove("dis-none");
+  // const adduserbtn = document.getElementById("add_user_btn");
+  // adduserbtn.addEventListener(
+  //   "click", () => {
+  //     document.querySelector(".add_user_modal").classList.add("show");
+  //     document.querySelector(".add_user_modal_back").classList.remove("dis-none");
 
-    })
-  document.querySelector(".add_user_modal_back").addEventListener("click", () => {
-    document.querySelector(".add_user_modal").classList.remove("show");
-    document.querySelector(".add_user_modal_back").classList.add("dis-none");
-  });
-
-
-
-  
-  // function getuserbyip () {
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //   .then((response) => response.json())
-  //   .then((json) => console.log(json));
-  // }
-   
-
-  // let html = "";
-  // for (var users in getuserbyip()) {
-
-
-  //   html = html + `<tr>
-  //                         <td>${users.id}</td>
-  //                         <td>${users.name}</td>
-  //                         <td>${users.username}</td>
-  //                         <td>${users.email}</td>
-  //                         <td>${users.address.city}</td>
-  //                         <td>
-  //                             <i
-  //                                 class="material-icons red-text m-l-5 m-r-5  ">delete_forever</i>
-  //                             <i
-  //                                 class="material-icons orange-text darken-3 m-l-5 m-r-5 ">edite</i>
-  //                         </td>
-  //                   </tr>`
-
-  // }
-
-  // document.getElementsByClassName("dekstop_table_body").innerHtml = html;
-
+  //   })
+  // document.querySelector(".add_user_modal_back").addEventListener("click", () => {
+  //   document.querySelector(".add_user_modal").classList.remove("show");
+  //   document.querySelector(".add_user_modal_back").classList.add("dis-none");
+  // });
 
 
 
